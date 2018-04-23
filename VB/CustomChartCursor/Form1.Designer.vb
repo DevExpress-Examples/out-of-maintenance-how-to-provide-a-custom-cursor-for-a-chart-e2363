75c6455157d948e26ba4c093d26b7546d25a96ec
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/CustomChartCursor/Form1.Designer.vb
@@ -0,0 +1,94 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Namespace CustomChartCursor
+	Partial Public Class Form1
+		''' <summary>
+		''' Required designer variable.
+		''' </summary>
+		Private components As System.ComponentModel.IContainer = Nothing
+
+		''' <summary>
+		''' Clean up any resources being used.
+		''' </summary>
+		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+			If disposing AndAlso (components IsNot Nothing) Then
+				components.Dispose()
+			End If
+			MyBase.Dispose(disposing)
+		End Sub
+
+		#Region "Windows Form Designer generated code"
+
+		''' <summary>
+		''' Required method for Designer support - do not modify
+		''' the contents of this method with the code editor.
+		''' </summary>
+		Private Sub InitializeComponent()
+			Dim xyDiagram1 As New DevExpress.XtraCharts.XYDiagram()
+			Dim series1 As New DevExpress.XtraCharts.Series()
+			Dim seriesPoint1 As New DevExpress.XtraCharts.SeriesPoint("A", New Object() { (CObj(12R))})
+			Dim seriesPoint2 As New DevExpress.XtraCharts.SeriesPoint("B", New Object() { (CObj(14R))})
+			Dim seriesPoint3 As New DevExpress.XtraCharts.SeriesPoint("C", New Object() { (CObj(16R))})
+			Dim seriesPoint4 As New DevExpress.XtraCharts.SeriesPoint("D", New Object() { (CObj(8R))})
+			Dim seriesPoint5 As New DevExpress.XtraCharts.SeriesPoint("E", New Object() { (CObj(9R))})
+			Dim seriesPoint6 As New DevExpress.XtraCharts.SeriesPoint("F", New Object() { (CObj(13R))})
+			Dim seriesPoint7 As New DevExpress.XtraCharts.SeriesPoint("G", New Object() { (CObj(11R))})
+			Dim seriesPoint8 As New DevExpress.XtraCharts.SeriesPoint("H", New Object() { (CObj(7R))})
+			Dim seriesPoint9 As New DevExpress.XtraCharts.SeriesPoint("I", New Object() { (CObj(15R))})
+			Dim seriesPoint10 As New DevExpress.XtraCharts.SeriesPoint("J", New Object() { (CObj(6R))})
+			Dim seriesPoint11 As New DevExpress.XtraCharts.SeriesPoint("K", New Object() { (CObj(13R))})
+			Dim seriesPoint12 As New DevExpress.XtraCharts.SeriesPoint("L", New Object() { (CObj(7R))})
+			Dim seriesPoint13 As New DevExpress.XtraCharts.SeriesPoint("M", New Object() { (CObj(16R))})
+			Dim seriesPoint14 As New DevExpress.XtraCharts.SeriesPoint("N", New Object() { (CObj(8R))})
+			Dim seriesPoint15 As New DevExpress.XtraCharts.SeriesPoint("O", New Object() { (CObj(15R))})
+			Dim seriesPoint16 As New DevExpress.XtraCharts.SeriesPoint("P", New Object() { (CObj(11R))})
+			Me.chartControl1 = New DevExpress.XtraCharts.ChartControl()
+			CType(Me.chartControl1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(xyDiagram1, System.ComponentModel.ISupportInitialize).BeginInit()
+			CType(series1, System.ComponentModel.ISupportInitialize).BeginInit()
+			Me.SuspendLayout()
+			' 
+			' chartControl1
+			' 
+			Me.chartControl1.CrosshairEnabled = DevExpress.Utils.DefaultBoolean.False
+			xyDiagram1.AxisX.VisibleInPanesSerializable = "-1"
+			xyDiagram1.AxisX.VisualRange.Auto = False
+			xyDiagram1.AxisX.VisualRange.MaxValueSerializable = "J"
+			xyDiagram1.AxisX.VisualRange.MinValueSerializable = "A"
+			xyDiagram1.AxisY.VisibleInPanesSerializable = "-1"
+			xyDiagram1.EnableAxisXScrolling = True
+			Me.chartControl1.Diagram = xyDiagram1
+			Me.chartControl1.Dock = System.Windows.Forms.DockStyle.Fill
+			Me.chartControl1.Legend.Visible = False
+			Me.chartControl1.Location = New System.Drawing.Point(0, 0)
+			Me.chartControl1.Name = "chartControl1"
+			series1.LabelsVisibility = DevExpress.Utils.DefaultBoolean.False
+			series1.Name = "Series 1"
+			series1.Points.AddRange(New DevExpress.XtraCharts.SeriesPoint() { seriesPoint1, seriesPoint2, seriesPoint3, seriesPoint4, seriesPoint5, seriesPoint6, seriesPoint7, seriesPoint8, seriesPoint9, seriesPoint10, seriesPoint11, seriesPoint12, seriesPoint13, seriesPoint14, seriesPoint15, seriesPoint16})
+			Me.chartControl1.SeriesSerializable = New DevExpress.XtraCharts.Series() { series1}
+			Me.chartControl1.Size = New System.Drawing.Size(463, 283)
+			Me.chartControl1.TabIndex = 0
+'			Me.chartControl1.QueryCursor += New DevExpress.XtraCharts.QueryCursorEventHandler(Me.chartControl1_QueryCursor);
+			' 
+			' Form1
+			' 
+			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+			Me.ClientSize = New System.Drawing.Size(463, 283)
+			Me.Controls.Add(Me.chartControl1)
+			Me.Name = "Form1"
+			Me.Text = "Form1"
+			CType(xyDiagram1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(series1, System.ComponentModel.ISupportInitialize).EndInit()
+			CType(Me.chartControl1, System.ComponentModel.ISupportInitialize).EndInit()
+			Me.ResumeLayout(False)
+
+		End Sub
+
+		#End Region
+
+		Private WithEvents chartControl1 As DevExpress.XtraCharts.ChartControl
+	End Class
+End Namespace
+
